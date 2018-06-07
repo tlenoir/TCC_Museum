01c357e7e01c202f2a28e75d001f9364c3451eb4
tlenoir <tlenoir.tcc@gmail.com>
2018-06-06 16:08:56 -1000

    redirect to Tabs

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -19,14 +19,15 @@ import { TabsPage } from '../tabs/tabs';
 export class WelcomePage {
 
   data: SQLiteObject;
+  progress = 0;
 
   constructor(public navCtrl: NavController,
     public navParams: NavParams,
     private sqlite: SQLite) {
 
-      this.initDB();
+    this.initDB();
   }
-  
+
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad WelcomePage');
@@ -50,7 +51,26 @@ export class WelcomePage {
     this.data.executeSql('CREATE TABLE IF NOT EXISTS oeuvres (id INTEGER PRIMARY KEY, lastname TEXT, firstname TEXT, image TEXT, code INTEGER, checked INTEGER)', {})
       .then(() => {
         console.log('Executed SQL');
+        let totalOeuvres = this.checkOeuvresExits();
+
+        if (totalOeuvres == 21) {
+          this.redirectToTabs();
+        } else {
+          
+        
         this.insertOeuvresData();
+      }
+      })
+      .catch(e => console.log(e));
+
+  }
+
+  checkOeuvresExits(): any {
+
+    return this.data.executeSql('SELECT * FROM oeuvres', {})
+      .then((data) => {
+        console.log("checkOeuvresExits");
+        return data.rows.lenght;
       })
       .catch(e => console.log(e));
 
@@ -60,17 +80,17 @@ export class WelcomePage {
 
     let inserts =
       "INSERT INTO `oeuvres` VALUES (1,'ALVAREZ','Jean-Pierre','9213750369.jpg',9213750369,0)," +
-      "(2,'ARAI','Poeragni','6510403686.jpg',6510403686,0)," +
-      "(3,'CHANSIN','Jerôme','7216899933.jpg',7216899933,0)," +
+      "(2,'ARAI','Poeragui','6510403686.jpg',6510403686,0)," +
+      "(3,'CHANSIN','Jérôme','7216899933.jpg',7216899933,0)," +
       "(4,'CHEUNG-SEN ','Jonas','1629568455.jpg',1629568455,0)," +
-      "(5,'CUNNY','Heimana','9266553664.jpg',9266553664,0)," +
+      "(5,'CUNY','Heimana','9266553664.jpg',9266553664,0)," +
       "(6,'EBB','Nicolas','1168085824.jpg',1168085824,0)," +
       "(7,'LEHARTEL','Alexandre','2791010818.jpg',2791010818,0)," +
       "(8,'LENOIR','Tetuaoro','4173047359.jpg',4173047359,0)," +
       "(9,'LONGINE','Manaarii ','9782420312.jpg',9782420312,0)," +
       "(10,'LY','Joane ','6872232276.jpg',6872232276,0)," +
       "(11,'MARO','Teremu ','1234567890.jpg',1234567890,0)," +
-      "(12,'MONACO','Vaitare','4653519064.jpg',4653519064,0)," +
+      "(12,'MONACO','Vaitiare','4653519064.jpg',4653519064,0)," +
       "(13,'PAEAHI','Ariipaea','3658034121.jpg',3658034121,0)," +
       "(14,'PAMBRUN','Aito ','5175547403.jpg',5175547403,0)," +
       "(15,'PAMBRUN','Hiomai','9520532017.jpg',9520532017,0)," +
@@ -84,11 +104,28 @@ export class WelcomePage {
     this.data.executeSql(inserts, {})
       .then(() => {
 
-        this.navCtrl.push(TabsPage);
+        this.redirectToTabs();
 
       })
       .catch((e) => console.log('error', e))
 
 
+  }
+  // is progressbar
+  redirectToTabs() {
+    let limit = 10;
+    let counter = 0;
+    let myInterval = setInterval(() => {
+      counter++;
+      console.log('count', counter);
+      this.progress = counter * 100 / limit;
+      console.log('progress', this.progress);
+
+      if (counter == limit) {
+        clearInterval(myInterval);
+        this.navCtrl.push(TabsPage);
+      }
+
+    }, 1000);
   }
 }
